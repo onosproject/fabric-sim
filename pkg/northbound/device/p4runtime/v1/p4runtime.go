d20d8205d6f3c72bd645d5ca827c2107f0919f76
Thomas Vachuska <tom.vachuska@intel.com>
2022-07-13 12:18:00 -0700

    Laying out the initial NB API and simulated device NB structures (#3)

---
+++ pkg/northbound/device/p4runtime/v1/p4runtime.go
@@ -0,0 +1,50 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+// Package gnmi implements the northbound gNMI service for the configuration subsystem.
+package gnmi
+
+import (
+	"context"
+	simapi "github.com/onosproject/onos-api/go/onos/fabricsim"
+	"github.com/onosproject/onos-lib-go/pkg/logging"
+	p4rtapi "github.com/p4lang/p4runtime/go/p4/v1"
+)
+
+var log = logging.GetLogger("northbound", "device", "p4runtime")
+
+// Server implements the P4Runtime API
+type Server struct {
+	deviceID simapi.DeviceID
+}
+
+func (s *Server) Write(ctx context.Context, request *p4rtapi.WriteRequest) (*p4rtapi.WriteResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *Server) Read(request *p4rtapi.ReadRequest, server p4rtapi.P4Runtime_ReadServer) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *Server) SetForwardingPipelineConfig(ctx context.Context, request *p4rtapi.SetForwardingPipelineConfigRequest) (*p4rtapi.SetForwardingPipelineConfigResponse, error) {
+	log.Infof("Device %s: Forwarding pipeline configuration has been set", s.deviceID)
+	panic("implement me")
+}
+
+func (s *Server) GetForwardingPipelineConfig(ctx context.Context, request *p4rtapi.GetForwardingPipelineConfigRequest) (*p4rtapi.GetForwardingPipelineConfigResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *Server) StreamChannel(server p4rtapi.P4Runtime_StreamChannelServer) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *Server) Capabilities(ctx context.Context, request *p4rtapi.CapabilitiesRequest) (*p4rtapi.CapabilitiesResponse, error) {
+	log.Infof("Device %s: P4Runtime capabilities have been requested", s.deviceID)
+	panic("implement me")
+}
---
+++ pkg/northbound/fabricsim/hosts.go
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: 2022-present Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package fabricsim
+
+import (
+	"context"
+	simapi "github.com/onosproject/onos-api/go/onos/fabricsim"
+)
+
+func (s *Server) GetHosts(ctx context.Context, request *simapi.GetHostsRequest) (*simapi.GetHostsResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *Server) GetHost(ctx context.Context, request *simapi.GetHostRequest) (*simapi.GetHostResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *Server) AddHost(ctx context.Context, request *simapi.AddHostRequest) (*simapi.AddHostResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *Server) RemoveHost(ctx context.Context, request *simapi.RemoveHostRequest) (*simapi.RemoveHostResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
