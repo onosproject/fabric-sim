ac3db146f4dcfd567221a6e9660385c65d76c281
Thomas Vachuska <tom.vachuska@intel.com>
2022-09-22 19:01:29 -0700

    Added positioning of nodes for superspine topology generation

--- pkg/topo/superspine_fabric.go
+++ pkg/topo/superspine_fabric.go
@@ -6,6 +6,11 @@ package topo
 
 import "fmt"
 
+const (
+	sspineY    = -200
+	sspinesGap = 800
+)
+
 // GenerateSuperSpineFabric generates topology YAML from the specified super-spine fabric recipe
 func GenerateSuperSpineFabric(fabric *SuperSpineFabric) *Topology {
 	log.Infof("Generating Super-Spine Fabric")
@@ -14,8 +19,10 @@ func GenerateSuperSpineFabric(fabric *SuperSpineFabric) *Topology {
 	builder := NewBuilder()
 
 	// First, create 2 super-spines
-	createSwitch("sspine1", 32, builder, topology, nil)
-	createSwitch("sspine2", 32, builder, topology, nil)
+	createSwitch("sspine1", 32, builder, topology,
+		pos(coord(1, 2, sspinesGap, 0), sspineY))
+	createSwitch("sspine2", 32, builder, topology,
+		pos(coord(2, 2, sspinesGap, 0), sspineY))
 
 	// Next generate two rack-pair fabrics and connect it to super-spines
 	createRackPairFabric(1, fabric, builder, topology)
@@ -27,8 +34,10 @@ func createRackPairFabric(rackID int, fabric *SuperSpineFabric, builder *Builder
 	// TODO: implement grid layout positioning
 
 	// First, create 2 spines
-	spine1 := createSwitch(fmt.Sprintf("spine%d", rackID), 32, builder, topology, nil).ID
-	spine2 := createSwitch(fmt.Sprintf("spine%d", rackID+1), 32, builder, topology, nil).ID
+	spine1 := createSwitch(fmt.Sprintf("spine%d", rackID), 32, builder, topology,
+		pos(coord(rackID, 4, 2*spinesGap, 0), spineY)).ID
+	spine2 := createSwitch(fmt.Sprintf("spine%d", rackID+1), 32, builder, topology,
+		pos(coord(rackID+1, 4, 2*spinesGap, 0), spineY)).ID
 
 	// Connect the spines to super-spines
 	createLinkTrunk(spine1, "sspine1", 8, builder, topology)
@@ -37,10 +46,14 @@ func createRackPairFabric(rackID int, fabric *SuperSpineFabric, builder *Builder
 	createLinkTrunk(spine2, "sspine2", 8, builder, topology)
 
 	// Next, create 2 sets of paired leaves
-	leaf11 := createSwitch(fmt.Sprintf("leaf%d1", rackID), 32, builder, topology, nil).ID
-	leaf12 := createSwitch(fmt.Sprintf("leaf%d2", rackID), 32, builder, topology, nil).ID
-	leaf21 := createSwitch(fmt.Sprintf("leaf%d1", rackID+1), 32, builder, topology, nil).ID
-	leaf22 := createSwitch(fmt.Sprintf("leaf%d2", rackID+1), 32, builder, topology, nil).ID
+	leaf11 := createSwitch(fmt.Sprintf("leaf%d1", rackID), 32, builder, topology,
+		pos(coord(2*rackID-1, 8, leafGap, 0), leafY)).ID
+	leaf12 := createSwitch(fmt.Sprintf("leaf%d2", rackID), 32, builder, topology,
+		pos(coord(2*rackID+0, 8, leafGap, 0), leafY)).ID
+	leaf21 := createSwitch(fmt.Sprintf("leaf%d1", rackID+1), 32, builder, topology,
+		pos(coord(2*rackID+1, 8, leafGap, 0), leafY)).ID
+	leaf22 := createSwitch(fmt.Sprintf("leaf%d2", rackID+1), 32, builder, topology,
+		pos(coord(2*rackID+2, 8, leafGap, 0), leafY)).ID
 
 	// Connect the leaves to the spines
 	createLinkTrunk(leaf11, spine1, 4, builder, topology)
@@ -57,6 +70,8 @@ func createRackPairFabric(rackID int, fabric *SuperSpineFabric, builder *Builder
 	createLinkTrunk(leaf21, leaf22, 2, builder, topology)
 
 	// Finally, create hosts with dual interfaces to the paired leaves
-	createRackHosts(rackID, leaf11, leaf12, 10, builder, topology, 0, hostsPerRow)
-	createRackHosts(rackID+1, leaf21, leaf22, 10, builder, topology, 0, hostsPerRow)
+	createRackHosts(rackID, leaf11, leaf12, 10, builder, topology,
+		coord(2*rackID-1, 4, leafGap, -3*leafGap/2), hostsPerRow)
+	createRackHosts(rackID+1, leaf21, leaf22, 10, builder, topology,
+		coord(2*rackID+0, 4, leafGap, -leafGap/2), hostsPerRow)
 }
